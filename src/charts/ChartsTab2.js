f5503de37862ee25e5cb6c2caa9fdd4293a4d5e4
Olexandr@MS <obal@dtu.dk>
2018-12-21 07:45:50 +0100

    Data update and more tabs

--- src/charts/ChartsTab2.js
+++ src/charts/ChartsTab2.js
@@ -28,31 +28,33 @@ const Charts = (props) => {
       {(props.scenarioSelection.showWelcome===true) && <Welcome closeWelcome={props.closeWelcome} />}
         {(props.scenarioSelection.showDifference===false || (props.scenarioSelection.showDifference===true && selectedScenario2==="")) && 
           <Flex>
-            <StackedBarChart chartName='_SUP_Energi forbrug i Danmark' chartTitle='Danmarks samlede energiforbrug' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={0} maxY={1000} />
-            <StackedBarChart chartName='_SUP_Olie og gas produktion' chartTitle='Olie og gas produktionen' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={0} maxY={500} />
-            <StackedBarChart chartName='_SUP_Biobrændsels forbrug' chartTitle='Biobrændselsforbruget' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={0} maxY={500} />
-            <StackedBarChart chartName='_SUP_Import_eksport biobrændsler' chartTitle='Import/eksport biomasse og biobrændsler' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={-40} maxY={220} />
-            <StackedBarChart chartName='_SUP_Forbrug VE raffinaderier' chartTitle='Brændselsforbrug på VE raffinaderier' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={0} maxY={300} />
-            <StackedBarChart chartName='_SUP_Bio-raffinaderi output' chartTitle='Bio-raffinaderi output' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={0} maxY={200} />
-            <StackedBarChart chartName='_SUP_El og varme sektorens brændse' chartTitle='El og varmesektorens brændselsforbrug' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={0} maxY={500} />
-            <StackedBarChart chartName='_SUP_Transport sektorens energifor' chartTitle='Transportsektorens energiforbrug' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={0} maxY={300} />
-            <StackedBarChart chartName='_SUP_Industriens energiforbrug' chartTitle='Industriens energiforbrug' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={0} maxY={500} />
-            <StackedBarChart chartName='_SUP_Husholdningers varmeforbrug' chartTitle='Husholdningernes energiforbrug' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={0} maxY={180} />
-          </Flex>
+            <StackedBarChart chartName='_Energi forbrug i Danmark' chartTitle='Danmarks samlede energiforbrug' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={0} maxY={1000} />
+            <StackedBarChart chartName='_Olie og gas produktion' chartTitle='Olie og gas produktionen' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={0} maxY={500} />
+            <StackedBarChart chartName='_Biobrændsels forbrug' chartTitle='Biobrændselsforbruget' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={0} maxY={500} />
+            <StackedBarChart chartName='_Import_eksport biobrændsler' chartTitle='Import/eksport biomasse og biobrændsler' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={-40} maxY={220} />
+            <StackedBarChart chartName='_Forbrug VE raffinaderier' chartTitle='Brændselsforbrug på VE raffinaderier' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={0} maxY={300} />
+            <StackedBarChart chartName='_Bio-raffinaderi output' chartTitle='Bio-raffinaderi output' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={0} maxY={200} />
+            <StackedBarChart chartName='_El og varme sektorens brænds' chartTitle='El og varmesektorens brændselsforbrug' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={0} maxY={500} />
+            <StackedBarChart chartName='_Transport sektorens energifo' chartTitle='Transportsektorens energiforbrug' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={0} maxY={300} />
+            <StackedBarChart chartName='_Industriens energiforbrug' chartTitle='Industriens energiforbrug' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={0} maxY={500} />
+            <StackedBarChart chartName='_Husholdningers varmeforbrug' chartTitle='Husholdningernes energiforbrug' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={0} maxY={180} />
+            <StackedBarChart chartName='_Produktion elektrofuels' chartTitle='Elektrofuels produktion' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label=' ' minY={0} maxY={50} />
+		  </Flex>
         }
         {(props.scenarioSelection.showDifference===true && selectedScenario2!=="") && 
           <Flex>
-            <StackedBarDiffChart chartName='_SUP_Energi forbrug i Danmark' chartTitle='Danmarks samlede energiforbrug' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={-200} maxY={200} />
-            <StackedBarDiffChart chartName='_SUP_Olie og gas produktion' chartTitle='Olie og gas produktionen' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={-100} maxY={100} />
-            <StackedBarDiffChart chartName='_SUP_Biobrændsels forbrug' chartTitle='Biobrændselsforbruget' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={-100} maxY={100} />
-            <StackedBarDiffChart chartName='_SUP_Import_eksport biobrændsler' chartTitle='Import/eksport biomasse og biobrændsler' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={-100} maxY={100} />
-            <StackedBarDiffChart chartName='_SUP_Forbrug VE raffinaderier' chartTitle='Brændselsforbrug på VE raffinaderier' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" label2="Netto eksport (PJ)" minY={-200} maxY={200} />
-            <StackedBarDiffChart chartName='_SUP_Bio-raffinaderi output' chartTitle='Bio-raffinaderi output' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={-100} maxY={100} />
-            <StackedBarDiffChart chartName='_SUP_El og varme sektorens brændse' chartTitle='El og varmesektorens brændselsforbrug' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={-200} maxY={200} />
-            <StackedBarDiffChart chartName='_SUP_Transport sektorens energifor' chartTitle='Transportsektorens energiforbrug' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={-100} maxY={100} />
-            <StackedBarDiffChart chartName='_SUP_Industriens energiforbrug' chartTitle='Industriens energiforbrug' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={-100} maxY={100} />
-            <StackedBarDiffChart chartName='_SUP_Husholdningers varmeforbrug' chartTitle='Husholdningernes energiforbrug' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={-10} maxY={10} />
-          </Flex>
+            <StackedBarDiffChart chartName='_Energi forbrug i Danmark' chartTitle='Danmarks samlede energiforbrug' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={-200} maxY={200} />
+            <StackedBarDiffChart chartName='_Olie og gas produktion' chartTitle='Olie og gas produktionen' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={-100} maxY={100} />
+            <StackedBarDiffChart chartName='_Biobrændsels forbrug' chartTitle='Biobrændselsforbruget' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={-100} maxY={100} />
+            <StackedBarDiffChart chartName='_Import_eksport biobrændsler' chartTitle='Import/eksport biomasse og biobrændsler' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={-100} maxY={100} />
+            <StackedBarDiffChart chartName='_Forbrug VE raffinaderier' chartTitle='Brændselsforbrug på VE raffinaderier' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" label2="Netto eksport (PJ)" minY={-200} maxY={200} />
+            <StackedBarDiffChart chartName='_Bio-raffinaderi output' chartTitle='Bio-raffinaderi output' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={-100} maxY={100} />
+            <StackedBarDiffChart chartName='_El og varme sektorens brænds' chartTitle='El og varmesektorens brændselsforbrug' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={-200} maxY={200} />
+            <StackedBarDiffChart chartName='_Transport sektorens energifo' chartTitle='Transportsektorens energiforbrug' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={-100} maxY={100} />
+            <StackedBarDiffChart chartName='_Industriens energiforbrug' chartTitle='Industriens energiforbrug' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={-100} maxY={100} />
+            <StackedBarDiffChart chartName='_Husholdningers varmeforbrug' chartTitle='Husholdningernes energiforbrug' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label="PJ" minY={-10} maxY={10} />
+            <StackedBarDiffChart chartName='_Produktion elektrofuels' chartTitle='Elektrofuels produktion' selectedScenario={selectedScenario} selectedScenario2={selectedScenario2} combinedChart={false} label=' ' minY={-10} maxY={30} />
+		  </Flex>
         }        
     </MainArea>
   );
--- src/data/scenarioCombinations.js
+++ src/data/scenarioCombinations.js
@@ -1,14 +1,13 @@
 export default {
-  scenarioCombinations : {
-    scenarioOptions : [
-      { "id": 0, "name": "DTU_ea18_fp", "short_description": "Energiaftalen 2018", "ultra_short_description": "Energiaftalen" },
-      { "id": 1, "name": "DTU_ea18_Regeringen", "short_description": "Regeringens klimaudspil", "ultra_short_description": "Regeringen" },
-      { "id": 2, "name": "DTU_ea18_sparenergi", "short_description": "DTU energispare", "ultra_short_description": "Energispare" },
-      { "id": 3, "name": "DTU_ea18_lav_financiel", "short_description": "DTU lav vÃ¦kst", "ultra_short_description": "Lav vÃ¦kst" },      
-      { "id": 4, "name": "division_line", "short_description": "_______________", "ultra_short_description": "_____" },
-      { "id": 5, "name": "DTU_ea18_CB20", "short_description": "Carbon budget 2Â°C", "ultra_short_description": "CB 2Â°C" },
-      { "id": 6, "name": "DTU_ea18_CB20_bio", "short_description": "Carbon budget 2Â°C, begr.bio", "ultra_short_description": "CB 2Â°C bio" },
-      { "id": 7, "name": "DTU_ea18_CB15_ccs", "short_description": "Carbon budget 1.5Â°C", "ultra_short_description": "CB 1Â½Â°C CSS" }
-    ]
-  }
+  scenarioCombinations : {scenarioOptions : [
+{ "id": 0, "name": "Climate_agreement", "short_description": "Climate Agreement", "ultra_short_description": "Climate" },
+{ "id": 1, "name": "Energispar", "short_description": "Energispare", "ultra_short_description": "Energispare" },
+{ "id": 2, "name": "Frozen_policy", "short_description": "Frozen policy", "ultra_short_description": "Frozen policy" },
+{ "id": 3, "name": "low_growth", "short_description": "Lav vækst", "ultra_short_description": "Lav vækst" },
+{ "id": 4, "name": "division_line", "short_description": "_______________", "ultra_short_description": "_____" },
+{ "id": 5, "name": "Carbon_scenario_1-5_degree_with_CCS", "short_description": "Carbon budget 1.5°C (CCS)", "ultra_short_description": "CB 1½°C CSS" },
+{ "id": 6, "name": "Carbon_scenario_1-5_degree_with_CCS_bio", "short_description": "Carbon budget 1.5°C (CCS_bio)", "ultra_short_description": "CB 1½°C CSS_bio" },
+{ "id": 7, "name": "Carbon_scenario_2-0_degree_with_CCS", "short_description": "Carbon budget 2°C (CCS)", "ultra_short_description": "CB 2°C CCS" },
+{ "id": 8, "name": "Carbon_scenario_2-0_degree_with_CCS_bio", "short_description": "Carbon budget 2°C (CCS_bio)", "ultra_short_description": "CB 2°C CCS_bio" }
+]}
 };

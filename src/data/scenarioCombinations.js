a5b83f38cb07f9baf6be021eda77d1c1c394c0a4
Olavur Ellefsen <olavur@Olavurs-MacBook-Pro.local>
2018-12-22 13:51:36 +0000

    Fixed danish characters in scenario names

--- src/data/scenarioCombinations.js
+++ src/data/scenarioCombinations.js
@@ -3,11 +3,11 @@ export default {
 { "id": 0, "name": "Climate_agreement", "short_description": "Climate Agreement", "ultra_short_description": "Climate" },
 { "id": 1, "name": "Energispar", "short_description": "Energispare", "ultra_short_description": "Energispare" },
 { "id": 2, "name": "Frozen_policy", "short_description": "Frozen policy", "ultra_short_description": "Frozen policy" },
-{ "id": 3, "name": "low_growth", "short_description": "Lav vækst", "ultra_short_description": "Lav vækst" },
+{ "id": 3, "name": "low_growth", "short_description": "Lav vÃ¦kst", "ultra_short_description": "Lav vÃ¦kst" },
 { "id": 4, "name": "division_line", "short_description": "_______________", "ultra_short_description": "_____" },
-{ "id": 5, "name": "Carbon_scenario_1-5_degree_with_CCS", "short_description": "Carbon budget 1.5°C (CCS)", "ultra_short_description": "CB 1½°C CSS" },
-{ "id": 6, "name": "Carbon_scenario_1-5_degree_with_CCS_bio", "short_description": "Carbon budget 1.5°C (CCS_bio)", "ultra_short_description": "CB 1½°C CSS_bio" },
-{ "id": 7, "name": "Carbon_scenario_2-0_degree_with_CCS", "short_description": "Carbon budget 2°C (CCS)", "ultra_short_description": "CB 2°C CCS" },
-{ "id": 8, "name": "Carbon_scenario_2-0_degree_with_CCS_bio", "short_description": "Carbon budget 2°C (CCS_bio)", "ultra_short_description": "CB 2°C CCS_bio" }
+{ "id": 5, "name": "Carbon_scenario_1-5_degree_with_CCS", "short_description": "Carbon budget 1.5Â°C (CCS)", "ultra_short_description": "CB 1Â½Â°C CSS" },
+{ "id": 6, "name": "Carbon_scenario_1-5_degree_with_CCS_bio", "short_description": "Carbon budget 1.5Â°C (CCS_bio)", "ultra_short_description": "CB 1Â½Â°C CSS_bio" },
+{ "id": 7, "name": "Carbon_scenario_2-0_degree_with_CCS", "short_description": "Carbon budget 2Â°C (CCS)", "ultra_short_description": "CB 2Â°C CCS" },
+{ "id": 8, "name": "Carbon_scenario_2-0_degree_with_CCS_bio", "short_description": "Carbon budget 2Â°C (CCS_bio)", "ultra_short_description": "CB 2Â°C CCS_bio" }
 ]}
 };

8e6db582564839240a95c364a576c7ba05e36780
Olavur Ellefsen <oe@tokni.com>
2019-03-31 18:41:59 +0100

    New setup of tests using react-testing-library

--- src/leftMenu/ToggleSwitch.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ToggleSwitch from './ToggleSwitch';
-
-it('renders the toggle switch', () => {
-  const wrapper = shallow(<ToggleSwitch />);
-  expect(wrapper.find('SwitchBoxAround')).toHaveLength(1);
-});
--- src/pages/ScenarioDescriptions.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ScenarioDescriptions from './ScenarioDescriptions';
-
-it('renders title', () => {
-  const wrapper = shallow(<ScenarioDescriptions />);
-  expect(wrapper.find('AboutHeader').children().text()).toEqual('Scenariebeskrivelser');
-});
